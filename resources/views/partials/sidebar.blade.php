774004219a71c9cfcaf3910be54ec1da81c370d2
Duhita <duhitashelly@gmail.com>
2025-09-20 14:55:22 +0700

    Build assets for deployment

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,7 +5,6 @@
   </button>
 </div>
 
-<!-- Sidebar (kode asli kamu) -->
 <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 
   overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl 
   dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" 
